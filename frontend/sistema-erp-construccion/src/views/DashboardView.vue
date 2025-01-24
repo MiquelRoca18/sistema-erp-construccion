67e7cd5de82c000d26887fbd0143c7354d1eb276
Miquel Roca <miquel1818@gmail.com>
2025-01-24 18:13:58 +0100

    Reinstall Vue & Tailwind, also First Steps with EmployeeProfileComponent

---
+++ frontend/sistema-erp-construccion/src/views/DashboardView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <!-- Employee Profile Component -->
+    <EmployeeProfile
+      employeePhoto="/path/to/photo.jpg"
+      employeeName="Juan Pérez"
+    />
+  </div>
+</template>
+
+<script>
+import EmployeeProfile from "../components/EmployeeProfile.vue";
+
+export default {
+  components: {
+    EmployeeProfile,
+  },
+};
+</script>
