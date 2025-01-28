7c2f2182b8c290443eeb7e7dfd0f0208e5c66baa
Miquel Roca <miquel1818@gmail.com>
2025-01-28 16:10:30 +0100

    Update CORS

--- backend/public/index.php
+++ backend/public/index.php
@@ -4,17 +4,17 @@
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
 
-    // Configuración de cabeceras CORS
-    header('Access-Control-Allow-Origin: *');
-    header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-    header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+    // // Configuración de cabeceras CORS
+    // header('Access-Control-Allow-Origin: *');
+    // header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+    // header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    // header("Allow: GET, POST, OPTIONS, PUT, DELETE");
 
-    // Si la solicitud es de tipo OPTIONS, responder inmediatamente con un 200 y terminar el script
-    if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") {
-        http_response_code(200);
-        exit;
-    }
+    // // Si la solicitud es de tipo OPTIONS, responder inmediatamente con un 200 y terminar el script
+    // if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") {
+    //     http_response_code(200);
+    //     exit;
+    // }
 
     // Cargar el autoload generado por Composer
     require_once __DIR__ . '/../vendor/autoload.php';
