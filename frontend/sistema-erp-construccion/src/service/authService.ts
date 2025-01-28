b6faeb772aded0ce79620fc7ac08084fab8dfbc4
Miquel Roca <miquel1818@gmail.com>
2025-01-28 16:55:33 +0100

    Better Configuration

--- frontend/sistema-erp-construccion/src/service/authService.ts
+++ frontend/sistema-erp-construccion/src/service/authService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost/sistema-erp-construccion/backend/public'; 
+const API_URL = import.meta.env.VITE_API_URL;
 
 interface LoginResponse {
   token: string;
