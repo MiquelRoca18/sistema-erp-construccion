ccfbed44385f8e778fb7b825517cfd94d8cdaf35
Miquel Roca <miquel1818@gmail.com>
2025-01-30 15:55:17 +0100

    Change Button Logout out of Component

--- frontend/sistema-erp-construccion/src/components/EmployeeProfileComponent.vue
+++ frontend/sistema-erp-construccion/src/components/EmployeeProfileComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col items-center bg-white p-6 rounded-lg shadow-md max-w-sm mx-auto cursor-pointer">
+  <div class="flex flex-col items-center">
     <!-- Foto del empleado -->
     <img
       class="w-32 h-32 rounded-full border-4 border-gray-300"
@@ -8,13 +8,6 @@
     />
     <!-- Nombre del empleado dinámico -->
     <h2 class="text-xl font-bold text-gray-800 mt-4">{{ employeeName }}</h2>
-    <!-- Botón de Logout -->
-    <button
-      @click="logout"
-      class="mt-6 px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600"
-    >
-      Logout
-    </button>
   </div>
 </template>
 
@@ -23,10 +16,4 @@ defineProps({
   employeeName: String,
   employeePhoto: String
 });
-
-const logout = () => {
-  localStorage.removeItem("token");
-  localStorage.removeItem("user");
-  window.location.href = "/"; 
-};
 </script>
--- frontend/sistema-erp-construccion/src/views/DashboardView.vue
+++ frontend/sistema-erp-construccion/src/views/DashboardView.vue
@@ -1,19 +1,27 @@
 <template>
-  <div>
+  <div class="flex flex-col items-center bg-white p-6 rounded-lg shadow-md max-w-sm mx-auto cursor-pointer">
     <router-link :to="`/employee/${employeeId}`">
       <EmployeeProfileComponent
         :employeePhoto="employeePhoto"
         :employeeName="employeeName"
       />
     </router-link>
+    <button
+      @click="logout"
+      class="mt-6 px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 items-center"
+    >
+      Logout
+    </button>
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue';
+import { useRouter } from 'vue-router';
 import { getEmployeeData } from '@/service/authService';
 import EmployeeProfileComponent from "@/components/EmployeeProfileComponent.vue";
 
+const router = useRouter();
 const employeeId = ref(null);
 const employeeName = ref("Empleado Desconocido");
 const employeePhoto = ref("/src/assets/images/employeePhoto.webp");
@@ -25,6 +33,14 @@ onMounted(async () => {
     employeeId.value = user;
     employeeName.value = employeeData.nombre || "Empleado Desconocido";
     employeePhoto.value = employeeData.photo || "/src/assets/images/employeePhoto.webp";
+  } else {
+    router.push('/');
   }
 });
+
+const logout = () => {
+  localStorage.removeItem("token");
+  localStorage.removeItem("user");
+  router.push('/'); 
+};
 </script>
